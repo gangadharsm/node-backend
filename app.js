58ae0f530009dc5f5d4292b5c398500ecb2c3638
gangadhara <gangadhara@bootminds.com>
2020-01-03 13:53:09 +0530

    Access-Control-Allow-Origin added

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
-app.get('/', (req, res) => res.json({name:'Gangadhara'}))
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user");
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
+
+app.use("/api/user", userRoutes);
 
 module.exports = app;
